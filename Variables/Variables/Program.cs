7a2c310329e996bf25ce40084a791c19c5dcfb65
ryanpatrck <ryanpatrck@gmail.com>
2018-08-26 18:34:07 -0400

    var practice

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,12 +6,12 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            byte number = 2;
-            int count = 10;
-            float totalPrice = 20.95f;
-            char character = 'A';
-            string firstName = "Ryan";
-            bool isWorking = false;
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Ryan";
+            var isWorking = false;
             Console.WriteLine(number);
             Console.WriteLine(count);
             Console.WriteLine(totalPrice);
