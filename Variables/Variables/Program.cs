489c8cc8b4a1ff5de9dc2fba405c8bbd09671a6b
ryanpatrck <ryanpatrck@gmail.com>
2018-08-26 18:41:41 -0400

    format strings

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,18 +6,7 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            var number = 2;
-            var count = 10;
-            var totalPrice = 20.95f;
-            var character = 'A';
-            var firstName = "Ryan";
-            var isWorking = false;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(character);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
 
         }
     }
