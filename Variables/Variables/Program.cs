512ac361d3ed10cd30574dc52b177818157947ec
ryanpatrck <ryanpatrck@gmail.com>
2018-08-26 18:26:46 -0400

    variable practice

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -7,6 +7,9 @@ namespace Variables
         static void Main(string[] args)
         {
             byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+
             Console.WriteLine(number);
         }
     }
