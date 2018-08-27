c7b7a23dbdd88e674fca2102a396c1535c49ac32
ryanpatrck <ryanpatrck@gmail.com>
2018-08-26 21:30:16 -0400

    operators

--- Operators/Operators/Program.cs
+++ Operators/Operators/Program.cs
@@ -9,8 +9,9 @@ namespace Operators
             var a = 1;
             var b = 2;
             var c = 3;
+           
 
-            Console.WriteLine(a + b * c);
+            Console.WriteLine(c > b && c > a );
         }
     }
 }
