e84eaaab39f0a9402bd36ae1b8250b4ea4bea56a
ryanpatrck <ryanpatrck@gmail.com>
2018-08-26 20:21:01 -0400

    conversions

--- Variables/Variables/Program.cs
+++ Variables/Variables/Program.cs
@@ -6,10 +6,9 @@ namespace Variables
     {
         static void Main(string[] args)
         {
-            byte b = 1;
-            int i = b;
-            Console.WriteLine(i);
-
+            int i = 1000;
+            byte b = (byte) i;
+            Console.WriteLine(b);
 
         }
     }
