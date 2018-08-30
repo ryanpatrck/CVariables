a5e5c97614fb4725f32e89685657514b1121f55e
ryanpatrck <ryanpatrck@gmail.com>
2018-08-30 13:19:59 -0400

    working with classes

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -1,22 +1,19 @@
-﻿using System;
+﻿using CSharpFundamentals.Math;
 
 namespace CSharpFundamentals
 {
-   public class Person
+    class Program
     {
-        public string FirstName;
-        public string LastName;
-
-        public void Introduce()
-        {
-            Console.WriteLine("My name is " + FirstName + " " + LastName);
-        }
         static void Main(string[] args)
         {
             var john = new Person();
             john.FirstName = "John";
             john.LastName = "Smith";
             john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            System.Console.WriteLine(result);
         }
     }
 }
