186248e74641ff617bde77e57780039acfd60620
ryanpatrck <ryanpatrck@gmail.com>
2018-08-31 13:57:55 -0400

    strings

--- Strings/Strings/Program.cs
+++ Strings/Strings/Program.cs
@@ -16,8 +16,12 @@ namespace Strings
 
             var names = new String[3]{ "John", "Jack", "Mary"};
             var formattedNames = string.Join(",", names);
-            Console.WriteLine(formattedNames);
 
+            var text = @"Hi John 
+Look into the following paths
+c:\folder1\folder2
+c:\folder3\folder4";
+            Console.WriteLine(text);
         }
     }
 }
