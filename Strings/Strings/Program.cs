56110870cbf56ad314c9e59f75f24a32b983d141
ryanpatrck <ryanpatrck@gmail.com>
2018-08-31 13:52:44 -0400

    strings

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var firstName = "Ryan";
+            var lastName = "Patrick";
+
+            var fullName = firstName + " " + lastName;
+
+            var myFullName = string.Format("My name is {0} {1}", firstName, lastName);
+
+            var names = new String[3]{ "John", "Jack", "Mary"};
+            var formattedNames = string.Join(",", names);
+            Console.WriteLine(formattedNames);
+
+        }
+    }
+}
