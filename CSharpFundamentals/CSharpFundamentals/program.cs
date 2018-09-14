68b594574e30ce63df24f4bbeeae6f916cebd8d3
ryanpatrck <ryanpatrck@gmail.com>
2018-09-14 12:18:32 -0400

    random class

---
+++ CSharpFundamentals/CSharpFundamentals/program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            for (var i = 0; i < 10; i++)
+                Console.WriteLine(random.Next());
+        }
+    }
+}
--- CSharpFundamentals/server.js
+++ CSharpFundamentals/server.js
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 namespace CSharpFundamentals
 {
     public class Person
