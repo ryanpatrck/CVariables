3cad4372fa61df2d5c0691294bc6259a0055107d
ryanpatrck <ryanpatrck@gmail.com>
2018-09-14 12:43:52 -0400

    random class

--- CSharpFundamentals/CSharpFundamentals/program.cs
+++ CSharpFundamentals/CSharpFundamentals/program.cs
@@ -7,8 +7,18 @@ namespace CSharpFundamentals
         static void Main(string[] args)
         {
             var random = new Random();
-            for (var i = 0; i < 10; i++)
-                Console.WriteLine(random.Next());
+
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+
+            for (var i = 0; i < passwordLength; i++)
+              buffer[i] = (char)('a' + random.Next(0, 26));
+
+            var password = new string(buffer);
+
+            Console.WriteLine(password);
+
         }
     }
 }
