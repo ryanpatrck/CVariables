f7bf2a7f56e30cc9f90a08e4e7a4558bf90b0823
ryanpatrck <ryanpatrck@gmail.com>
2018-09-19 21:07:02 -0400

    Exercise 3

---
+++ ForLoops/ForLoops/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+            for (var i = 1; i <= number; i++)
+                factorial *= i;
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+        }
+
+    }
+}
