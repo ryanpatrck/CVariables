ead41b2a7634822309b127ceb99122394a1aa128
ryanpatrck <ryanpatrck@gmail.com>
2018-09-24 11:45:40 -0400

    array methods

--- CSharpFundamentals/CSharpFundamentals/program.cs
+++ CSharpFundamentals/CSharpFundamentals/program.cs
@@ -6,18 +6,48 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var random = new Random();
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
 
-            const int passwordLength = 10;
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
 
-            var buffer = new char[passwordLength];
+            //IndexOf()
+            var index = Array.IndexOf(numbers, 9);
 
-            for (var i = 0; i < passwordLength; i++)
-              buffer[i] = (char)('a' + random.Next(0, 26));
+            Console.WriteLine("Index of 9: " + index);
 
-            var password = new string(buffer);
+            //Clear() setting items to null
+            Array.Clear(numbers, 0, 2);
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
 
-            Console.WriteLine(password);
+            // Copy()
+            int[] another = new int[3];
+            Array.Copy(numbers, another, 3);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Sort()
+            Array.Sort(numbers);
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Reverse()
+            Array.Reverse(numbers);
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
 
         }
     }
