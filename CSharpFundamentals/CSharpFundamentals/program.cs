2e5e95babf3332b704eeaf6c42ff76c0d0b8e55d
ryanpatrck <ryanpatrck@gmail.com>
2018-10-08 17:17:16 -0400

    facebook assignment

--- CSharpFundamentals/CSharpFundamentals/program.cs
+++ CSharpFundamentals/CSharpFundamentals/program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharpFundamentals
 {
@@ -6,48 +7,29 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+            var numbers = new List<int>() { 1, 2, 3, 4 };
+            numbers.Add(1);
+            numbers.AddRange(new int[3] { 5, 6, 7});
 
-            // Length
-            Console.WriteLine("Length: " + numbers.Length);
+            foreach ( var number in numbers)
+                Console.WriteLine(number);
+            Console.WriteLine();
+            Console.WriteLine("Index of 1: " + numbers.IndexOf(1));
+            Console.WriteLine("Index of 1: " + numbers.LastIndexOf(1));
 
-            //IndexOf()
-            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Count: " + numbers.Count);
 
-            Console.WriteLine("Index of 9: " + index);
-
-            //Clear() setting items to null
-            Array.Clear(numbers, 0, 2);
-            Console.WriteLine("Effect of Clear()");
-            foreach (var n in numbers)
+            for (var i = 0; i < numbers.Count; i++ )
             {
-                Console.WriteLine(n);
+                if (numbers[i] == 1)
+                numbers.Remove(numbers[i]);
             }
+            foreach (var number in numbers)
+        // Not allowed to modify a collection in a foreach loop
+            Console.WriteLine(number);
 
-            // Copy()
-            int[] another = new int[3];
-            Array.Copy(numbers, another, 3);
-            Console.WriteLine("Effect of Copy()");
-            foreach (var n in another)
-            {
-                Console.WriteLine(n);
-            }
-
-            // Sort()
-            Array.Sort(numbers);
-            Console.WriteLine("Effect of Sort()");
-            foreach (var n in numbers)
-            {
-                Console.WriteLine(n);
-            }
-
-            // Reverse()
-            Array.Reverse(numbers);
-            Console.WriteLine("Effect of Reverse()");
-            foreach (var n in numbers)
-            {
-                Console.WriteLine(n);
-            }
+            numbers.Clear();
+            Console.WriteLine("Count: " + numbers.Count);
 
         }
     }
